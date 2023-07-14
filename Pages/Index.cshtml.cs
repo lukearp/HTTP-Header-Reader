62e5392ee35318155b53a02a7b61c0e5d65ff5f2
Luke Arp <ruarp@microsoft.com>
2023-07-14 12:27:02 -0400

    base commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HTTP_Header_Reader.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+    public IHeaderDictionary headers {get;set;}
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var context = HttpContext;
+        headers = context.Request.Headers;
+    }
+}
